b07033de16091f4d6f1d164e06518fd24579386e
Ruoli <angrl@zen.com.my>
2020-07-10 17:54:50 +0800

    Delete path name

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { LoginComponent } from '../app/login/login.component';
 
 
 const routes: Routes = [
-  { path: 'tryadmin', component: LoginComponent }
+  { path: '', component: LoginComponent }
 ];
 
 
